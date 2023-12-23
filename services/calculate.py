9f108b073b266567ff978e8ff80ae796132ac7cb
Dimam <Dimaman12@yandex.by>
2023-12-23 07:22:01 +0300

    add fix

--- services/calculate.py
+++ services/calculate.py
@@ -1,7 +1,7 @@
 # Функция: calculate_average(numbers)
 def calculate_average(numbers):
     if len(numbers) == 0:
-        print("sadf")
+        print()
         return None
     return sum(numbers) / len(numbers)
 
--- test_my.py
+++ test_my.py
@@ -1,6 +1,5 @@
 
 import pytest
-from services.calculate import *
 from services.factorial import *
 from services.palindrome import *
 from services.priority import *
@@ -33,9 +32,3 @@ def test_is_palindrome():
     assert is_palindrome("level") == True
     assert is_palindrome("python") == False
     assert is_palindrome("") == True
-
-# Тесты для функции calculate_average(numbers)
-def test_calculate_average():
-    assert calculate_average([1, 2, 3, 4, 5]) == 3.0
-    assert calculate_average([10, 20, 30, 40, 50]) == 30.0
-    assert calculate_average([]) == None
