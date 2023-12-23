1c8ec8957e6891d3ad3f9ceca5d1510ce1f1f287
Dimam <Dimaman12@yandex.by>
2023-12-23 07:22:09 +0300

    add fix

---
+++ test_cal.py
@@ -0,0 +1,6 @@
+from services.calculate import *
+# Тесты для функции calculate_average(numbers)
+def test_calculate_average():
+    assert calculate_average([1, 2, 3, 4, 5]) == 3.0
+    assert calculate_average([10, 20, 30, 40, 50]) == 30.0
+    assert calculate_average([]) == None
