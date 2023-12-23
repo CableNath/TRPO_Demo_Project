ab848e0bb10938fef579c1cc17a20c28ee142bd4
Dimam <Dimaman12@yandex.by>
2023-12-23 07:29:39 +0300

    add fix

--- services/calculate.py
+++ services/calculate.py
@@ -1,7 +1,6 @@
 # Функция: calculate_average(numbers)
 def calculate_average(numbers):
     if len(numbers) == 0:
-        print()
         return None
     return sum(numbers) / len(numbers)
 
--- services/palindrome.py
+++ services/palindrome.py
@@ -1,4 +1,5 @@
 
 # Функция: is_palindrome(s)
 def is_palindrome(s):
+    print("ad")
     return s == s[::-1]
